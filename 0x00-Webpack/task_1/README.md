0cdf4926e5dbec0e17bb567b272aefad436a5fc8
yam1st <mantynouyacoubou@gmail.com>
2024-10-16 04:11:47 -0700

    Deploying changes

---
+++ 0x00-Webpack/task_1/README.md
@@ -0,0 +1,2 @@
+Learning how to use Webpack with a config file
+mandatory
---
+++ 0x01-react_intro/task_1/dashboard/src/utils.js
@@ -0,0 +1,8 @@
+// utils.js
+export function getFullYear() {
+  return new Date().getFullYear();
+}
+
+export function getFooterCopy(isIndex) {
+  return isIndex ? 'Holberton School' : 'Holberton School main dashboard';
+}
